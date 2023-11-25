87c918981ae2468a84431c8ec96271afec2f4ef0
Claudiu-eng <tulbureclaudiu2001@gmail.com>
2023-11-25 13:06:31 +0200

    implementing second assigment

---
+++ src/main/java/com/example/ds2023_30641_tulbure_claudiu_marcel_1_monitoringcommunicationmanagement/Ds202330641TulbureClaudiuMarcel1MonitoringcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ds2023_30641_tulbure_claudiu_marcel_1_monitoringcommunicationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds202330641TulbureClaudiuMarcel1MonitoringcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ds202330641TulbureClaudiuMarcel1MonitoringcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ds2023_30641_tulbure_claudiu_marcel_1_monitoringcommunicationmanagement/Ds202330641TulbureClaudiuMarcel1MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ds2023_30641_tulbure_claudiu_marcel_1_monitoringcommunicationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds202330641TulbureClaudiuMarcel1MonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
